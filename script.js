d74f603a342c014633de2bf17dadb77b78264895
Ray <105173457+RayIssam@users.noreply.github.com>
2022-08-28 22:54:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//fetch videos from youtube API
 const url="https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC98Suvqo2atFD0Y6e3Bn2AA&maxResults=3&order=date&key=AIzaSyCM4gJwvXZYvpf-EtgY7snempe8q8P4Drs"
 fetch(url)
 .then(response => {return response.json() })
@@ -6,10 +7,10 @@ fetch(url)
 function youtubeLatestVideos(data){
     console.log(data)
 data.forEach(element => {
-    let video= element.snippet.title;
-    let title=element.snippet.thumbnails.title;
+  //  let video= element.snippet.title;
+ //   let title=element.snippet.thumbnails.title;
     let videoId=element.id.videoId;
-    let image=element.snippet.thumbnails.high.url;
+   // let image=element.snippet.thumbnails.high.url;
     document.getElementById("latest-videos").innerHTML +=
       `<div> <iframe src="https://www.youtube.com/embed/${videoId}"></iframe></div>`
 });
